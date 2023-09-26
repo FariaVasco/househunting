88e4a3664386960c4d72738e72ede24c93effb35
vascolencastredefaria <fariavasco96@gmail.com>
2023-09-26 18:30:57 +0200

    Fixed path to existing_houses

--- new_houses.py
+++ new_houses.py
@@ -4,7 +4,7 @@ import config as cfg
 
 def add_new_houses(new_links):
 
-    existing_houses = open(os.path.join(cfg.root_dir, 'helpers/existing_houses.txt', 'a'))
+    existing_houses = open(os.path.join(cfg.root_dir, 'helpers/existing_houses.txt'), 'a')
 
     for house in new_links:
         existing_houses.write("\n" + house['url'])
