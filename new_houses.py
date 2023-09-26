3b716344def6960c6df4714f3f98b3607c45153b
vascolencastredefaria <fariavasco96@gmail.com>
2023-09-26 18:29:43 +0200

    Adding root_dir

--- new_houses.py
+++ new_houses.py
@@ -1,6 +1,10 @@
+import os
+import config as cfg
+
+
 def add_new_houses(new_links):
 
-    existing_houses = open('helpers/existing_houses.txt', 'a')
+    existing_houses = open(os.path.join(cfg.root_dir, 'helpers/existing_houses.txt', 'a'))
 
     for house in new_links:
         existing_houses.write("\n" + house['url'])
